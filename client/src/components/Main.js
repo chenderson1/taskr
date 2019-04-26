de7ca27d0bb7b412e4c2b6ec4652472a81735c39
Reg Garrett <regsg8@gmail.com>
2019-04-26 17:55:18 -0600

    Landing Update

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,20 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Landing from './Landing'
 import Dashboard from './Dashboard'
+import axios from 'axios'
 
-const Main = () => {
+class Main extends Component {
+  constructor(){
+    super()
+    this.state = {
+      username: '',
+      isLoggedIn: false,
+      userId: '',
+      fName: '',
+      lName: '',
+    }
+  }
+
+  //Working, Used on Landing -> Login
+    //Saves user data to State
+  handleChange = (e) => {
+    const { name, value } = e.target
+    e.persist()
+    this.setState({
+      [name]: value
+    })
+  }
+
+  //Not working, Used on Landing -> Login
+  loginUser = (e) => {
+    const { name, value } = e.target
+    e.preventDefault()
+    e.persist()
+    //Add axios request to find user _id by username, prompts sign up if no match for username.  If successful, saves user data to state, changes loggedIn to true
+    this.setState()
+  }
+
+  //Not working, Used on Landing -> SignUp
+  registerUser = (e) => {
+    const { name, value } = e.target
+    //Add axios request to find if username is taken.  If successful, creates a new user using "User" model, takes them to Login.
+  }
+
+  render() {
     const styles = {
-        mainDiv: {
-          border: 'solid blue 1px',
-          margin: '5px'
+          mainDiv: {
+            border: 'solid blue 1px',
+            margin: '5px'
+          }
         }
-      }
     return (
         <div style={styles.mainDiv}>
-            <Landing />
+            <Landing loginUser={this.loginUser} handleChange={this.handleChange} />
             <Dashboard />
         </div>
     )
+  } 
 }
 
 export default Main 
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SignUp = props => {
+    const styles = {
+        signUpDiv: {
+            width: '300px',
+            height: '200px',
+            border: 'solid black 1px'
+        }
+    }
+    return (
+        <div style={styles.signUpDiv}>
+            This gets displayed on 'Sign Up' button click from Login component<br></br>
+            Sign Up <br></br>
+            First Name: <input name='fName' placeholder='Enter first name' onChange={props.handleChange}></input> <br></br>
+            Last Name: <input name='lName' placeholder='Enter last name' onChange={props.handleChange}></input> <br></br>
+            Username: <input name='username' placeholder='Enter username' onChange={props.handleChange}></input> <br></br>
+            <button>Back</button><button>Sign Up</button>
+        </div>
+    )
+}
+
+export default SignUp
