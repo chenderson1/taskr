94a68551c5ca0e14b9acb1de1e7ba8502bb34976
Cory Henderson <cory.henderson07@gmail.com>
2019-05-02 13:36:41 -0600

    added context that provides user and token on signup/login, tested , before merge

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,6 +4,14 @@ import Dashboard from "./dashboard/Dashboard";
 import axios from "axios";
 import Nav from "./nav/Nav";
 import Header from "./Header";
+import { withData } from "../context/dataContext";
+const TaskrAxios = axios.create();
+
+TaskrAxios.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 class Main extends Component {
   constructor() {
@@ -20,31 +28,22 @@ class Main extends Component {
       User: {
         boards: []
       },
-      selectedBoard: '',
+      selectedBoard: "",
       quote: "",
       isEdit: false,
-      updateThisBoard: "",
-     
+      updateThisBoard: ""
     };
   }
 
-  //Not Working, used in axios requests to find user by username
-  findUsername = () => {
-    console.log("findUsername called");
-    // axios.get(`api/userRoutes/${this.state.username}`)
-    //   .then(res => {
-    //     this.setState({ User: res.data })
-    //   })
-    //   .catch(err => console.log(err.response.data.errMsg))
-  };
-
   //Working, Used on Nav -> Login / SignUp
-    //Saves user data to State, calls updateNewUser
-  handleChange = (e) => {
-    const { name, value } = e.target
-    e.persist()
-    this.setState({ [name]: value }, () => {this.updateNewUser()})
-  }
+  //Saves user data to State, calls updateNewUser
+  handleChange = e => {
+    const { name, value } = e.target;
+    e.persist();
+    this.setState({ [name]: value }, () => {
+      this.updateNewUser();
+    });
+  };
 
   //Working, called by handleChange, saves state data to newUser
   updateNewUser = () => {
@@ -54,23 +53,32 @@ class Main extends Component {
         username: this.state.username,
         password: this.state.password
       }
-    })
-  }
+    });
+  };
 
   // Working, Used on Nav -> SignUp
-  registerUser = () => {
-    axios.post('/api/users', this.state.newUser)
-      .then(res => {
-        this.setState({ 
-          User: res.data,
-          isLoggedIn: true
-        })
-      })
-      .catch(err => console.log(err.response.data.errMsg))
-  }
+  registerUser = async e => {
+    e.preventDefault();
+    try {
+      const res = await this.props.signup(this.state.newUser);
+      this.setState({
+        User: res.data,
+        isLoggedIn: true
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  //Not working, Used on Nav -> Login
-  loginUser = () => {
+  //working, Used on Nav -> Login
+  loginUser = async e => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    const res = await this.props.login({
+      username: username,
+      password: password
+    });
+    console.log(res.data);
     this.setState({ isLoggedIn: true });
     //Use the below code when routes are working:
     // this.findUsername()
@@ -78,18 +86,25 @@ class Main extends Component {
     //If true, saves User data to state and changes loggedIn to true
   };
 
-  //Temporary toggle to swap between greeting and taskview
-  logoutUser = () => {
-    this.setState({ isLoggedIn: false });
-  };
-
-  //Not working, Used on Landing -> SignUp
-  registerUser = () => {
-    this.setState({ User: {} }); //Clears User field to make ternary work properly
-    this.findUsername();
-    this.User !== {}
-      ? axios.post("/api/userRoutes").then(this.loginUser()) //If true, POST and login
-      : alert("Sorry, that username is taken, please choose a different one!"); //If false, prompt user to choose a new username
+  logoutUser = async () => {
+    const res = await this.props.logout();
+    this.setState({
+      isLoggedIn: false,
+      username: "",
+      password: "",
+      userId: "",
+      fName: "",
+      lName: "",
+      display: true,
+      newUser: {},
+      User: {
+        boards: []
+      },
+      selectedBoard: "",
+      quote: "",
+      isEdit: false,
+      updateThisBoard: ""
+    });
   };
 
   //Working, Used on Landing to toggle login/signup displays
@@ -104,14 +119,14 @@ class Main extends Component {
   // Works, gets the quote of the
   // day and displays it in the header after login
   getQuote = () => {
-    axios.get("http://quotes.rest/qod.json").then(res => {
+    TaskrAxios.get("http://quotes.rest/qod.json").then(res => {
       const quote = res.data.contents.quotes[0].quote;
       this.setState({ quote: quote });
     });
   };
   // NOT working - gets boards by user id
   getUserBoards = () => {
-    axios.get(`/api/boards/5cc7adabc7f653c7458489ca`).then(res => {
+    TaskrAxios.get(`/api/boards/5cc7adabc7f653c7458489ca`).then(res => {
       const data = res.data;
       this.setState(ps => {
         return {
@@ -125,7 +140,7 @@ class Main extends Component {
   };
 
   deleteBoard = id => {
-    axios.delete(`/api/boards/board/${id}`);
+    TaskrAxios.delete(`/api/boards/board/${id}`);
     this.getUserBoards();
   };
 
@@ -140,31 +155,32 @@ class Main extends Component {
   };
 
   editBoard = boardToEdit => {
-    axios.put(`/api/boards/board/${boardToEdit._id}`, boardToEdit)
-    .then(res => {
-      console.log(res.data)
-      this.getUserBoards();
-    })
-  }
+    TaskrAxios.put(`/api/boards/board/${boardToEdit._id}`, boardToEdit).then(
+      res => {
+        console.log(res.data);
+        this.getUserBoards();
+      }
+    );
+  };
 
   // "Add Board" functionality
   boardHandleSubmit = boardName => {
-    if(boardName._id){
-     return this.editBoard(boardName)
+    if (boardName._id) {
+      return this.editBoard(boardName);
     }
-    axios.post("/api/boards/5cc7adabc7f653c7458489ca", boardName).then(res => {
-      console.log(res.data);
-      this.getUserBoards();
-    });
+    TaskrAxios.post("/api/boards/5cc7adabc7f653c7458489ca", boardName).then(
+      res => {
+        console.log(res.data);
+        this.getUserBoards();
+      }
+    );
   };
   componentDidMount() {
     this.getQuote();
     this.getUserBoards();
   }
 
-  addTask = () => {
-    
-  }
+  addTask = () => {};
 
   render() {
     const props = {
@@ -201,4 +217,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withData(Main);
