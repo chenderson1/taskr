7c7dd7ff8561cde4e4dd83906e3a7434eabd88d9
Cory Henderson <cory.henderson07@gmail.com>
2019-05-03 14:25:24 -0600

    before merge

--- client/src/components/landing/Greeting.js
+++ client/src/components/landing/Greeting.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import taskr from '../../resources/taskr.png'
-import WhyTaskr from './WhyTaskr'
+import React from "react";
+import taskr from "../../resources/taskr.png";
+import WhyTaskr from "./WhyTaskr";
 
 const Greeting = () => {
-    const styles = {
-        img: {
-            height: '100px',
-            margin: '10px'
-        },
-        greetingDiv: {
-            padding: '10px',
-            margin: '10px'
-        }
+  const styles = {
+    img: {
+      height: "100px",
+      margin: "10px"
+    },
+    greetingDiv: {
+      padding: "10px",
+      margin: "10px"
     }
-    return (
-        <div>
-            <img src={taskr} alt="dinosaur icon" style={styles.img}/>
-            
-            <div style={styles.greetingDiv}>
-                Welcome! <br></br> Meet task'r, the taskosaurus-rex. <br></br>
-                He needs your help, task'r is very hungry - complete tasks so he can eat!
-            </div>
-            <br></br><br></br>
-            <WhyTaskr />
-        </div>
-    )
-}
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: "#fbfbfb",
+        borderRadius: "5px"
+      }}
+    >
+      <img src={taskr} alt="dinosaur icon" style={styles.img} />
 
-export default Greeting
+      <div style={styles.greetingDiv}>
+        <span
+          style={{
+            color: "#fea42a",
+            textTransform: "uppercase",
+            fontSize: "5rem",
+            fontFamily: "'Just Another Hand', 'cursive'"
+          }}
+        >
+          {" "}
+          Welcome!{" "}
+        </span>{" "}
+        <br /> Meet task'r, the taskosaurus-rex. <br />
+        He needs your help, task'r is very hungry - complete tasks so he can
+        eat!
+      </div>
+      <br />
+      <br />
+      <WhyTaskr />
+    </div>
+  );
+};
+
+export default Greeting;
